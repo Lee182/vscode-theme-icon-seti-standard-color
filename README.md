aae4485b5249482c19c56f0c87bfd21f419e0689
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2021-01-06 11:11:36 +0000

    up Readme

--- README.md
+++ README.md
@@ -15,13 +15,19 @@ Modify args.js
 module.exports = {
   color: '#666',
   copyToPackageJson: {
-    name: 'vscode-theme-seti-standard-color',
+    name: 'vscode-theme-seti-666',
+    version: '21.1.6',
     license: 'MIT',
     publisher: 'lee182',
     repository: {
       type: 'git',
-      url: 'https://github.com/Lee182/vscode-theme-icon-seti-666'
+      url: 'https://github.com/Lee182/vscode-theme-seti-666'
     }
+  },
+  packageNLS: {
+    displayName: 'Seti Desaturated #666',
+    description: 'Seti theme with standard color #666 desaturated gray color',
+    themeLabel: 'Seti Desaturated #666'
   }
 }
 ```
--- args.js
+++ args.js
@@ -1,17 +1,18 @@
 module.exports = {
   color: '#666',
   copyToPackageJson: {
-    name: 'vscode-theme-seti-standard-color',
+    name: 'vscode-theme-seti-666',
+    version: '21.1.6',
     license: 'MIT',
     publisher: 'lee182',
     repository: {
       type: 'git',
-      url: 'https://github.com/Lee182/vscode-theme-icon-seti-666'
+      url: 'https://github.com/Lee182/vscode-theme-seti-666'
     }
   },
   packageNLS: {
-    displayName: 'Seti File Icon Theme #666',
-    description: 'A file icon theme made out of the Seti UI file icons',
-    themeLabel: 'Seti (Visual Studio Code) #666'
+    displayName: 'Seti Desaturated #666',
+    description: 'Seti theme with standard color #666 desaturated gray color',
+    themeLabel: 'Seti Desaturated #666'
   }
 }
